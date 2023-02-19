f76807eb8f95b7e60c71289af051fb142f0644a9
josMadriaga <jose_estudiante_@hotmail.com>
2023-02-19 00:13:12 -0300

    comienzo a estructurar el menu

--- src/madriaga/jose/editor/Principal.java
+++ src/madriaga/jose/editor/Principal.java
@@ -1,10 +1,13 @@
 package madriaga.jose.editor;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextPane;
+import javax.swing.JPopupMenu.Separator;
 
 public class Principal {
     public static void main(String[] args) {
@@ -13,6 +16,7 @@ public class Principal {
         marco.setVisible(true);//siempre al final para que cargue todo antes de visualizar
     }
 }
+
 class Marco extends JFrame{
     public Marco(){
         setBounds(300, 300, 300, 300);
@@ -23,14 +27,71 @@ class Marco extends JFrame{
 }
 class Panel extends JPanel{
     public Panel(){
+        //---------------men---------------------
+        JPanel panelMenu=new JPanel();
+        menu =new JMenuBar();
+
+        archivo=new JMenu("Archivo");
+        editar=new JMenu("Editar");
+        seleccion=new JMenu("Seleccción");
+        ver=new JMenu("Ver");
+        apariencia= new JMenu("Apariencia");
+
+        menu.add(archivo);
+        menu.add(editar);
+        menu.add(seleccion);
+        menu.add(ver);
+
+       //------------Elementos del menu archivo----------------
+        creaItem("Nuevo Archivo","archivo","nuevo");
+        creaItem("Abrir Archivo","archivo","abrir");
+        editar.addSeparator();
+        creaItem("Guardar","archivo","guardar");
+        creaItem("Guardar Como","archivo","guardarComo");
+        //--------------------------------------------------------
+        //-----------Elementos del manu editar------------------
+        creaItem("Deshacer","editar","");
+        creaItem("Rehacer","editar","");
+        editar.addSeparator();
+        creaItem("Cortar","editar","");
+        creaItem("Copiar","editar","");
+        creaItem("Pegar","editar","");
+        //------------------------------------------------------
+        //------------Elementos del menu seleccion---------------
+        creaItem("Seleccionar","seleccionar","");
+        //-------------------------------------------------------
+        //------------Elementos del menu ver---------------------
+        creaItem("Numeracion","ver","");
+        editar.addSeparator();
+        ver.add(apariencia);
+        creaItem("Normal","apariencia","");
+        creaItem("Dark","apariencia","");
+        //-------------------------------------------------------
+        panelMenu.add(menu);
+        //--------------------------------------
 
         //------------------Area de texto-----
         tPane=new JTabbedPane();
 
         //------------------------------------
 
+        add(panelMenu);
         add(tPane);
         
+    }
+
+    public void creaItem(String rotulo, String menu, String accion){
+        elementoItem=new JMenuItem(rotulo);
+
+        if(menu.equals("archivo")){
+            archivo.add(elementoItem);
+        }else if(menu.equals("editar")){
+            editar.add(elementoItem);
+        }else if(menu.equals("seleccion")){
+            seleccion.add(elementoItem);
+        }else if(menu.equals("ver")){
+            ver.add(elementoItem);
+        }
     }
 
     public void creaPanel(){
@@ -44,5 +105,8 @@ class Panel extends JPanel{
     private JTabbedPane tPane;
     private JPanel ventana;
     private JTextPane areaTexto;
+    private JMenuBar menu;
+    private JMenu archivo,editar,seleccion,ver,apariencia;
+    private JMenuItem elementoItem;
 
 }
